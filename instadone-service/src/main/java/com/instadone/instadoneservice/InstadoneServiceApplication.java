3f2f39838ce4d317a38cf75054f84cf55c821135
padumales <jenitta.kumploliljose@standardbank.co.za>
2018-01-16 00:34:25 +0530

    created project architecture

---
+++ instadone-service/src/main/java/com/instadone/instadoneservice/InstadoneServiceApplication.java
@@ -0,0 +1,12 @@
+package com.instadone.instadoneservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstadoneServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstadoneServiceApplication.class, args);
+	}
+}
